251afcbe57b7c4a97a2027af423909c484872032
Rolando Antón <ranton@fortinet.com>
2019-11-05 21:41:54 -0500

    Update startup.sh
    When you remove these options:
    
    -ncache 10 -ncache_cr
    
    It no longer show a tall page.

--- startup.sh
+++ startup.sh
@@ -5,5 +5,5 @@ Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 #openbox-session&
 xfce4-session&
-x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+x11vnc -display :1 -nopw -listen localhost -xkb -forever &
 cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
